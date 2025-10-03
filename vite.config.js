c34813daf187e3d7379d61b71e6aec8770344d9b
marned91 <152280051+marned91@users.noreply.github.com>
2025-10-03 18:05:00 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
+        auth: resolve(__dirname, 'auth/index.html'),
         login: resolve(__dirname, 'auth/login/index.html'),
         register: resolve(__dirname, 'auth/register/index.html'),
         profile: resolve(__dirname, 'profile/index.html'),
