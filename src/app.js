e93eedaa23b00a9ce129f05d1a9bba45267ca68b
marned91 <152280051+marned91@users.noreply.github.com>
2025-10-03 15:21:07 +0300

    trying to deploy with netlify

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import './css/styles.css';
 import router from './js/router';
+
 (async () => {
   await router(window.location.pathname);
 })();
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/CSS-Frameworks/',
-  build: { outDir: 'docs' },
+  base: '/',
+  build: { outDir: 'dist' },
 });
